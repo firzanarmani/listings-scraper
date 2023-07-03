5260fc14daf86586e06f391f0e871e775b922c52
Firzan Armani <mrfirzanarmani@gmail.com>
2023-07-03 21:50:11 +0800

    remove graphql query in slugString

--- src/utils/slugStringDB.ts
+++ src/utils/slugStringDB.ts
@@ -1,8 +1,6 @@
-// import { gql } from "graphql-request";
 import { customAlphabet } from "nanoid";
 import { numbers } from "nanoid-dictionary";
 import slugify from "slugify";
-// import { gqlServerClient } from "../helpers/graphqlClient";
 
 const randomNumbers = customAlphabet(numbers, 5);
 
@@ -16,33 +14,6 @@ export const slugStringDB = async (
     strict: true,
     trim: true,
   });
-
-  // TODO Remove on release
-  // if (id) {
-  //   const currentSlugResults = await gqlServerClient.request(
-  //     gql`
-  //       query GetSlug($id: uuid!) {
-  //         ${table}_by_pk(id: $id) {
-  //           slug
-  //         }
-  //       }
-  //     `,
-  //     {
-  //       id,
-  //     }
-  //   );
-
-  //   const currentSlug = currentSlugResults?.[`${table}_by_pk`]?.slug;
-
-  //   if (currentSlug) {
-  //     const currentSlugSplit = currentSlug.split("-");
-  //     currentSlugSplit.pop();
-  //     const currentSlugWithoutRandomNumbers = currentSlugSplit.join("-");
-  //     if (currentSlugWithoutRandomNumbers === slugifiedValue) {
-  //       return currentSlug;
-  //     }
-  //   }
-  // }
 
   return `${slugifiedValue}-${randomNumbers()}`;
 };
