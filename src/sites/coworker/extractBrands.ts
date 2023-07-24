3c88c2128f9440dda0e3fe521038066f1a248cd6
Firzan Armani <mrfirzanarmani@gmail.com>
2023-07-24 10:51:04 +0800

    add missing extractBrands

---
+++ src/sites/coworker/extractBrands.ts
@@ -0,0 +1,35 @@
+import { CompleteSpace } from "./types";
+import {
+  groupDuplicates,
+  groupSplitHyphen,
+  removeSameLinks,
+} from "../../utils/group";
+
+const extractBrands = (spaces: CompleteSpace[]) => {
+  let allSpaces = spaces;
+
+  allSpaces = allSpaces.sort((a, b) => {
+    if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+    if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+    return 0;
+  });
+
+  const listings: Record<string, CompleteSpace[]> = {};
+
+  allSpaces = removeSameLinks(allSpaces);
+
+  allSpaces = groupDuplicates(allSpaces, listings);
+
+  allSpaces = groupSplitCommas(allSpaces, listings);
+
+  allSpaces = groupSplitHyphen(allSpaces, listings);
+
+  // Naively add everything else
+  allSpaces.forEach((space) => {
+    listings[space.name] = [space];
+  });
+
+  return listings;
+};
+
+export default extractBrands;
