f2f54893aed8d83d81871fb440789da46f92e986
Firzan Armani <mrfirzanarmani@gmail.com>
2023-08-17 19:17:15 +0800

    update scraper to add filmplace

--- src/scraper/index.ts
+++ src/scraper/index.ts
@@ -1,11 +1,17 @@
 import { ListingsProvider } from "../types/constants";
 import { scrapeCoworker } from "../sites/coworker/scraper";
+import { scrapeFilmplace } from "../sites/filmplace/scraper";
+import { Cities } from "../constants";
 
-const scrape = async (provider: ListingsProvider, cityCode: string) => {
+const scrape = async (provider: ListingsProvider, cityCode: Cities) => {
   let data: Record<string, any[]> = {};
+
   switch (provider) {
     case "coworker":
       data = await scrapeCoworker(cityCode);
+      break;
+    case "filmplace":
+      data = await scrapeFilmplace(cityCode);
       break;
     case "allospaces":
       break;
